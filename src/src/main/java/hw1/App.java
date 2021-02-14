f44219c1114744d34a8d01076fdb549c2489e6c8
Alex Patsalov <alexpatsalov@gmail.com>
2021-02-14 18:43:25 +0200

    hw1 done

--- src/src/main/java/hw1/App.java
+++ src/src/main/java/hw1/App.java
@@ -1,18 +1,29 @@
 package hw1;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
 
-    public static Boolean isInt (String s){
-        try {
-            Integer.parseInt(s);
-            return true;
-        }
-        catch(NumberFormatException e) {
+    public static Boolean isInt(String s) {
+        if (s == null) {
             return false;
         }
+        int length = s.length();
+        if (length == 0) {
+            return false;
+        }
+        if (s.charAt(0) == '-' && length == 1) {
+            return false;
+        }
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -21,18 +32,26 @@ public class App {
         Scanner name = new Scanner(System.in);
         String userName = name.nextLine();
         Random random = new Random();
-        int randomNumber = random.nextInt(100) + 1;
+        int randomNumber = random.nextInt(10) + 1;
         Scanner number = new Scanner(System.in);
         System.out.print("Guess a number? \n");
-        for (; ;){
-            int userNumber = number.nextInt();
-            if (userNumber < randomNumber){
-                System.out.print("It is too small, try again: \n");
-            } else if (userNumber > randomNumber){
-                System.out.print("It is too big, try again: \n");
+        for (; ; ) {
+            String userNumber = number.nextLine();
+            if (isInt(userNumber)) {
+                int input = Integer.parseInt(userNumber);
+                if (input > 100){
+                    System.out.print("The range is < 100 \nTry again: \n");
+                }
+                else if (input < randomNumber) {
+                    System.out.print("It is too small, try again: \n");
+                } else if (input > randomNumber) {
+                    System.out.print("It is too big, try again: \n");
+                } else {
+                    System.out.printf("Ta-da correct! Well done, %s \n", userName);
+                    break;
+                }
             } else {
-                System.out.printf("Ta-da correct! Well done, %s", userName);
-                break;
+                System.out.print("Input correct value: \n");
             }
         }
     }
