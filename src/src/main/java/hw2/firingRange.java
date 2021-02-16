9358c3aa6b6c54a7d7affea8892fffa3e9c164a8
Alex Patsalov <alexpatsalov@gmail.com>
2021-02-16 21:17:00 +0200

    hw2 done

--- src/src/main/java/hw2/firingRange.java
+++ src/src/main/java/hw2/firingRange.java
@@ -1,7 +1,7 @@
 package hw2;
 
-import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class firingRange {
@@ -16,23 +16,43 @@ public class firingRange {
         for (String a : arr){
             joiner.add(String.format("%s", a));
         }
-
         return joiner.toString();
     }
 
     public static void main(String[] args) {
+        int[] target = {randomNumber(), randomNumber()};
         int rows = 6;
         int cols = 6;
+        boolean endOfGame = false;
+        Scanner sc = new Scanner(System.in);
         String [][] array = new String[rows][cols];
-            for (int i = 0; i < rows; i++){
-                for (int j = 0; j < cols; j++){
+        for (; ;) {
+            System.out.print("Type row: ");
+            int userRow = sc.nextInt();
+            System.out.print("Type col: ");
+            int userCol = sc.nextInt();
+            System.out.println();
+            if (userRow == target[0] && userCol == target[1]){
+                array[target[0]][target[1]] = "X";
+                endOfGame = true;
+            } else {
+                array[userRow][userCol] = "o";
+                System.out.print("Try again \n");
+            }
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
                     array[0][j] = "" + j;
                     array[i][0] = "" + i;
-                    if (array[i][j] == null){
+                    if (array[i][j] == null) {
                         array[i][j] = "-";
                     }
                 }
                 System.out.println(arrayToString(array[i]));
             }
+            if(endOfGame) {
+                System.out.print("You win! \n");
+                break;
+            }
+        }
     }
 }
