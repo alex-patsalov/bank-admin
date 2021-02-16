92d90297c3ff1cd2373b6903f53c44662ffcf48b
Alex Patsalov <alexpatsalov@gmail.com>
2021-02-16 20:40:54 +0200

    hw2 coming

--- src/src/main/java/hw2/firingRange.java
+++ src/src/main/java/hw2/firingRange.java
@@ -11,10 +11,10 @@ public class firingRange {
         return random.nextInt(5) + 1;
     }
 
-    public static String arrayToString (int [] arr){
+    public static String arrayToString (String[] arr){
         StringJoiner joiner = new StringJoiner(" | ");
-        for (int a : arr){
-            joiner.add(String.format("%d", a));
+        for (String a : arr){
+            joiner.add(String.format("%s", a));
         }
 
         return joiner.toString();
@@ -23,13 +23,13 @@ public class firingRange {
     public static void main(String[] args) {
         int rows = 6;
         int cols = 6;
-        int[][] array = new int[rows][cols];
+        String [][] array = new String[rows][cols];
             for (int i = 0; i < rows; i++){
                 for (int j = 0; j < cols; j++){
-                    array[0][j] = j;
-                    array[i][0] = i;
-                    if (i > 0 && j == 0){
-                        array[i][j] = '-';
+                    array[0][j] = "" + j;
+                    array[i][0] = "" + i;
+                    if (array[i][j] == null){
+                        array[i][j] = "-";
                     }
                 }
                 System.out.println(arrayToString(array[i]));
