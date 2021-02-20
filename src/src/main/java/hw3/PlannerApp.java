365a589fe48d5f9d5b39c640e16cfc6e7fb10772
Alex Patsalov <alexpatsalov@gmail.com>
2021-02-20 19:15:45 +0200

    hw3 coming

--- src/src/main/java/hw3/PlannerApp.java
+++ src/src/main/java/hw3/PlannerApp.java
@@ -5,34 +5,40 @@ import java.util.Scanner;
 
 public class PlannerApp {
 
-    public static String[][] fillArray(String[] days, String[] tasks){
+    public static String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    public static String[] tasks = {
+            "Go to classes", "Go to work", "Go shopping", "Go home", "Do hometask", "Relax", "Work like hell"
+    };
+    public static Scanner sc = new Scanner(System.in);
+
+    public static String[][] fillArray(String[] days, String[] tasks) {
         String[][] schedule = new String[days.length][2];
-        for (int i = 0; i < days.length; i++){
+        for (int i = 0; i < days.length; i++) {
             schedule[i] = new String[]{days[i], tasks[i]};
         }
         return schedule;
     }
 
     public static void main(String[] args) {
-        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-        String[] tasks = {
-                "Go to classes", "Go to work", "Go shopping", "Go home", "Do hometask", "Relax", "Work like hell"
-        };
         String[][] schedule = fillArray(days, tasks);
-        Scanner sc = new Scanner(System.in);
+        System.out.println(Arrays.deepToString(schedule));
         System.out.print("Pick a day: ");
-        String userInput = sc.nextLine();
+        String userInput = sc.nextLine().trim();
+        boolean found = true;
         for (String[] element : schedule) {
-//            for (int j = 0; j < 2; j++){
-            if (userInput.equalsIgnoreCase(element[0])) {
-                System.out.printf("Your task for %s is %s \n", element[0], element[1]);
-            } else if (userInput.equalsIgnoreCase("exit")) {
+            if (userInput.equalsIgnoreCase("exit")) {
                 System.out.print("Bye!");
                 break;
-            } else {
-                System.out.print("I do not understand, try again later \n");
             }
-//            }
+            if (userInput.equalsIgnoreCase(element[0]) && found) {
+                System.out.printf("Your task for %s is %s \n", element[0], element[1]);
+                continue;
+            }
+            if (!userInput.equalsIgnoreCase(element[0])) {
+                continue;
+//                System.out.print("I do not understand, try again later \n");
+            }
         }
     }
 }
+
