f63b134697d9a05cb42bd3178e05d29f5424e7bb
Alex Patsalov <alexpatsalov@gmail.com>
2021-02-21 19:00:42 +0200

    hw4 coming

--- src/src/main/java/hw4/Human.java
+++ src/src/main/java/hw4/Human.java
@@ -1,5 +1,7 @@
 package hw4;
 
+import java.util.Arrays;
+
 public class Human {
 
     private String name;
@@ -12,23 +14,59 @@ public class Human {
     private String[][] schedule;
 
     public void greetPet(){
-        System.out.printf("Привет %s", pet.nickname);
+        System.out.printf("Привет %s", pet.getNickname());
     }
 
     public void describePet(){
         String trickLevel;
-        trickLevel = pet.trickLevel > 50 ? "очень хитрый" : "почти не хитрый";
+        trickLevel = pet.getTrickLevel() > 50 ? "очень хитрый" : "почти не хитрый";
 //        if (pet.trickLevel > 50) {
 //            trickLevel = "очень хитрый";
 //        } else {
 //            trickLevel = "почти не хитрый";
 //        }
-        System.out.printf("У меня есть %s, ему %d, он %s", pet.species, pet.age, trickLevel);
+        System.out.printf("У меня есть %s, ему %d, он %s", pet.getSpecies(), pet.getAge(), trickLevel);
     }
 
     public String toString(){
+        return String.format("%s{name='%s', surname='%s', year='%d', iq='%d', mother='%s', father='%s', pet='%s', habits=%s", this.getClass(), this.name, this.surname, this.yearOfBirth, this.iq,  this.mother.name + this.mother.surname, this.father.name + this.father.surname, pet.toString(), Arrays.toString(this.schedule));
+    }
 
-//        dog{nickname='Rock', age=5, trickLevel=75, habits=[eat, drink, sleep]}, где dog - вид животного;
+    public Human(String name, String surname, int yearOfBirth){
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public Human(String name, String surname, int yearOfBirth, Human mother, Human father){
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.father = father;
+        this.mother = mother;
+    }
+
+    public Human(String name, String surname, int yearOfBirth, Human mother, Human father, Pet pet, int iq, String[][] schedule){
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.father = father;
+        this.mother = mother;
+        this.pet = pet;
+        this.iq = iq;
+        this.schedule = schedule;
+    }
+
+    public Human(){
 
     }
+
+    public String getName(){return this.name;}
+    public String setName(String newName){return this.name = newName;}
+    public String getSurname(){return this.surname;}
+    public String setSurname(String newSurname){return this.surname = newSurname;}
+    public int getYearOfBirth(){return this.yearOfBirth;}
+    public int setYearOfBirth(int newYear){return this.yearOfBirth = newYear;}
+    public String getPet(Pet pet){return pet.toString();}
+    public Pet setPet(Pet pet){return this.pet = pet;}
 }
--- src/src/main/java/hw4/Pet.java
+++ src/src/main/java/hw4/Pet.java
@@ -1,5 +1,7 @@
 package hw4;
 
+import java.util.Arrays;
+
 public class Pet {
     private String species;
     private String nickname;
@@ -7,15 +9,58 @@ public class Pet {
     private int trickLevel; // 0...100 //
     private String[] habits;
 
-    public void eat(){
+    public void eat() {
         System.out.print("Я кушаю!");
     }
 
-    public void respond(){
+    public void respond() {
         System.out.printf("Привет хозяин. Я - %s. Я соскучился!", this.nickname);
     }
 
-    public void foul(){
+    public void foul() {
         System.out.print("Нужно хорошо замести следы");
     }
+
+    public String toString() {
+        return String.format("%s{nickname='%s', age=%d, tricklevel=%d, habits=%s", this.species, this.nickname, this.age, this.trickLevel, Arrays.toString(this.habits));
+    }
+
+    public Pet(String species, String nickname) {
+        this.species = species;
+        this.nickname = nickname;
+    }
+
+    public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = age;
+        this.trickLevel = trickLevel;
+        this.habits = habits;
+    }
+
+    public Pet() {
+
+    }
+
+    public String getNickname(){
+        return this.nickname;
+    }
+
+    public String setNickName(String newNickName){return this.nickname = newNickName;}
+
+    public int getTrickLevel(){
+        return this.trickLevel;
+    }
+
+    public int setTrickLevel(int newTrickLevel){
+        return this.trickLevel = newTrickLevel;
+    }
+
+    public int getAge(){return this.age;}
+
+    public int setAge(int newAge){
+        return this.age = newAge;
+    }
+
+    public String getSpecies(){return this.species;}
 }
